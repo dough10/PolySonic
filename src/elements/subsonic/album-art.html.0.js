bb9c37296b4cbd582af281c95fb6d7c3f426bf97
dough10 <doughten.jimi@gmail.com>
2015-03-21 10:59:30 -0500

    animate fav icon while it is waiting on response from server

--- src/elements/subsonic/album-art.html.0.js
+++ src/elements/subsonic/album-art.html.0.js
@@ -209,20 +209,40 @@ Polymer('album-art', {
 
   addFavorite: function (event, detail, sender) {
     'use strict';
+    var animation = new CoreAnimation();
+    animation.duration = 1000;
+    animation.iterations = 'Infinity';
+    animation.keyframes = [
+      {opacity: 1},
+      {opacity: 0}
+    ];
+    animation.target = sender;
+    animation.play();
     var url = this.url + "/rest/star.view?u=" + this.user + "&p=" + this.pass + "&f=json&v=" + this.version + "&c=PolySonic&albumId=" + sender.attributes.ident.value;
     this.tmpl.doXhr(url, 'json', function (e) {
       if (e.target.response['subsonic-response'].status === 'ok') {
         this.isFavorite = true;
+        animation.cancel();
       }
     }.bind(this));
   },
 
   removeFavorite: function (event, detail, sender) {
     'use strict';
+    var animation = new CoreAnimation();
+    animation.duration = 1000;
+    animation.iterations = 'Infinity';
+    animation.keyframes = [
+      {opacity: 1},
+      {opacity: 0}
+    ];
+    animation.target = sender;
+    animation.play();
     var url = this.url + "/rest/unstar.view?u=" + this.user + "&p=" + this.pass + "&f=json&v=" + this.version + "&c=PolySonic&albumId=" + sender.attributes.ident.value;
     this.tmpl.doXhr(url, 'json', function (e) {
       if (e.target.response['subsonic-response'].status === 'ok') {
         this.isFavorite = false;
+        animation.cancel();
       }
     }.bind(this));
   },
