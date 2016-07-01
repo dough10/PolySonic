41d7a151167cf82d57fe7c6bc532bc5cd2a00435
dough10 <doughten.jimi@gmail.com>
2016-07-01 16:35:23 -0500

    scaffolding

--- src/js/PolySonic.es6
+++ src/js/PolySonic.es6
@@ -1,5 +1,5 @@
 (function () {
-
+  const app = document.querySelector('#app');
 
   function _transitionElement(el, transition, transform, opacity, willChange) {
     return new Promise(resolve => {
@@ -42,36 +42,182 @@
       var element = albums[i];
       _transitionElement(
         element,
-        'all 200ms ' + delay + 'ms ease-in',
+        'all 150ms ' + delay + 'ms ease-in',
         'translateY(0px)',
         1,
         'transform opacity'
       );
     }
   }
+  
+  function goToPage(page) {
+    // configure main page animations
+    switch (true) {
+        
+      // going to setting page
+      case (page === 3):
+        app.entryAnimation = 'slide-from-bottom-animation';
+        app.exitAnimation = 'fade-out-animation';
+        break;
+        
+      // comming from settings page
+      case (app.page === 3):
+        app.entryAnimation = 'fade-in-animation';
+        app.exitAnimation = 'slide-down-animation';
+        break;
+        
+      // app other pages
+      case (page < app.page):
+        app.entryAnimation = 'slide-from-left-animation';
+        app.exitAnimation = 'slide-right-animation';
+        break;
+      case (page > app.page):
+        app.entryAnimation = 'slide-from-right-animation';
+        app.exitAnimation = 'slide-left-animation';
+        break;
+    }
+    app.page = page;
+  }
 
   function setRoute(hash) {
+    app.lastHash = location.hash;
     location.hash = hash;
   }
 
   function hashChangeCallback() {
     const hash = location.hash.split('/');
-    console.log(hash)
-    // switch (hash[0]) {
-    //
-    // }
+    switch (hash[0]) {
+      case '':
+        setRoute('#Albums');
+        return;
+        break;
+      case '#Albums':
+        goToPage(0);
+        app.subPage = 0;
+        break;
+      case '#Artists':
+        goToPage(0);
+        app.subPage = 1;
+        break;
+      case '#Podcasts':
+        goToPage(0);
+        app.subPage = 2;
+        break;
+      case '#Settings':
+        goToPage(3)
+        break;
+    }
   }
+  
+  function testConnection(obj) {
+    return new Promise((resolve, reject) => {
+      var subsonic = new SubsonicAPI(obj);
+      const readyCallback = function (event) {
+        document.removeEventListener('subsonicApi-ready', readyCallback);
+        if (event.detail.status === 'ok') {
+          resolve(subsonic);
+        } else {
+          reject();
+        }
+      };
+      document.addEventListener('subsonicApi-ready', readyCallback);
+    });
+  }
+  
+  /**
+   * upgrade old connection config with the new needed info
+   *
+   * @param {Object} oldConfig
+   */
+  function updateConfig(oldConfig) {
+    var currentConfig = oldConfig;
+    if (currentConfig.url) {
+      var url = new URL(currentConfig.url);
+      currentConfig.ip = url.host;
+      if (url.protocol === "https:") {
+        currentConfig.https = true;
+        if (!currentConfig.port) {
+          currentConfig.port = 443;
+        }
+      } else {
+        currentConfig.https = false;
+        if (!currentConfig.port) {
+          currentConfig.port = 80;
+        }
+      }
+      delete currentConfig.url;
+      if (!currentConfig.md5Auth) {
+        currentConfig.md5Auth = true;
+      }
+    }
+    return currentConfig;
+  }
+  
+  app.openDrawer = function () {
+    app.$.appDrawer.openDrawer();
+  };
+  
+  app.settings = function () {
+    app.$.appDrawer.closeDrawer();
+    setRoute('#Settings');
+  };
+  
+  app.back = function () {
+    setRoute(app.lastHash);
+  };
 
   window.addEventListener('hashchange', hashChangeCallback);
+  
+  app.addEventListener('dom-change', _ => {
+    hashChangeCallback();
+    app.dataLoading = false;
+    simpleStorage.getSync().then(syncStorage => {
+      console.log(syncStorage);
+      if (!syncStorage) {
+        // display login dialog
+        return;
+      }
+      app.configs = syncStorage.configs;
+      simpleStorage.getLocal().then(local => {
+        //console.log(local)
+        if (!local) {
+          // display login dialog
+          return;
+        }
+        app.currentConfig = local.currentConfig || 0;
+        app.bitRate = local.bitRate || '320';
+        const currentConfig = updateConfig(app.configs[app.currentConfig]);
+        app.configs[app.currentConfig] = currentConfig;
+        simpleStorage.setSync({
+          configs: app.configs
+        })
+        testConnection({
+          https: currentConfig.https,
+          ip: currentConfig.ip,
+          port: currentConfig.port,
+          user: currentConfig.user,
+          password: currentConfig.pass,
+          appName: 'PolySonic',
+          md5Auth: currentConfig.md5Auth
+        }).then(subsonic => {
+          app.subsonic = subsonic;
+          app.subsonic[syncStorage.request](syncStorage.sortType , 30, 0, (function (storage) {
+            if (syncStorage.mediaFolder !== 'none') {
+              return storage.mediaFolder;
+            }
+            return;
+          })(syncStorage)).then(firstResponse => {
+            app.albums = firstResponse;
+            return;
+          }).then(showApp).then(cascadeElements).catch(err => console.error(err));
+        }).catch(_ => {
+          // display login doalog
+        });
+        
+      });
 
-  window.onload = _ => {
-    var app = document.querySelector('#app');
-    app.page = 0;
-    app.albums = [
-      0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,
-      0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,
-      0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20
-    ];
-    setTimeout(_ => showApp().then(cascadeElements), 1000);
-  };
+    });
+  });
+
+
 })();
---
+++ src/js/simpleStorage.js
@@ -0,0 +1,210 @@
+/**
+ * title: simpleStorage
+ * author: Jimmy Doughten
+ * version: 0.1
+ * 2015
+ *
+ * a simple storage wrapper that will give 1 api for dealing with saving / getting settings in web apps.
+ * intended to make it easy to code for 1 api then use in either a chrome packaged app or on the web.
+ *
+ * a chrome packaged app will use the chome.storage api while non chrome apps will fallback to localStorage
+ *
+ *
+ */
+window.simpleStorage = (function () {
+  var simpleStorage = {
+
+    /**
+     * feature detect chrome app features
+     */
+    isChromeApp: 'chrome' in window && 'storage' in chrome,
+
+    /**
+     * feature detect localStorage
+     */
+    hasLocalStorage: 'localStorage' in window,
+
+    /**
+     * chrome.storage.sync api if avaliable fallback to localStorage
+     * saves are synced to chrome account and shared between sessions
+     * returns a promise with a data object
+     *
+     * @param {String} item - optional* key to look up
+     *
+     * @example
+     * simpleStorage.getSync().then(function (data) {
+     *   console.log(data);
+     * });
+     *
+     * get one
+     * simpleStorage.getSync('thing').then(function (data) {
+     *   // data will be the value of localStorage.thing or chrome.storage equivilant
+     *   console.log(data);
+     * });
+     */
+    getSync: function (item) {
+      return new Promise(function (resolve, reject) {
+        if (this.isChromeApp) {
+          chrome.storage.sync.get(function (cStorage) {
+            var thing;
+            if (item) {
+              thing = cStorage[item];
+            } else {
+              thing = cStorage;
+            }
+            resolve(thing);
+          });
+        } else if (this.hasLocalStorage) {
+          if (item) {
+            try {
+              var data = JSON.parse(localStorage[item]);
+              if (data.type === typeof data.value) {
+                resolve(data.value);
+              }
+            } catch(e) {
+              resolve(localStorage[item]);
+            }
+          } else {
+            var newObj = {};
+            for (var key in localStorage) {
+              try {
+                var singleData = JSON.parse(localStorage[key]);
+                if (singleData.type === typeof singleData.value) {
+                  newObj[key] = singleData.value;
+                }
+              } catch(e) {
+                newObj[key] = localStorage[key];
+              }
+            }
+            resolve(newObj);
+          }
+        } else {
+          console.warn('localStorage is not supported in your browser');
+        }
+      }.bind(this));
+    },
+
+    /**
+     * chrome.storage.local api if avaliable fallback to localStorage
+     * saves are local only
+     * returns a promise with a data object
+     *
+     * @param {String} item - optional* key to look up
+     *
+     * @example
+     * get all items
+     * simpleStorage.getLocal().then(function (data) {
+     *   console.log(data);
+     * });
+     *
+     * get one
+     * simpleStorage.getLocal('thing').then(function (data) {
+     *   // data will be the value of localStorage.thing or chrome.storage equivilant
+     *   console.log(data);
+     * });
+     */
+    getLocal: function (item) {
+      return new Promise(function (resolve, reject) {
+        if (this.isChromeApp) {
+          chrome.storage.local.get(function (cStorage) {
+            var thing;
+            if (item) {
+              thing = cStorage[item];
+            } else {
+              thing = cStorage;
+            }
+            resolve(thing);
+          });
+        } else if (this.hasLocalStorage) {
+          if (item) {
+            try {
+              var singleData = JSON.parse(localStorage[item]);
+              if (singleData.type === typeof singleData.value) {
+                resolve(singleData.value);
+              }
+            } catch(e) {
+              resolve(localStorage[item]);
+            }
+          } else {
+            var newObj = {};
+            for (var key in localStorage) {
+              try {
+                var data = JSON.parse(localStorage[key]);
+                if (data.type === typeof data.value) {
+                  newObj[key] = data.value;
+                }
+              } catch(e) {
+                newObj[key] = localStorage[key];
+              }
+            }
+            resolve(newObj);
+          }
+        } else {
+          console.warn('localStorage is not supported in your browser');
+        }
+      }.bind(this));
+    },
+
+    /**
+     * chrome.storage.sync api if avaliable fallback to localStorage
+     * saves are synced to chrome account and shared between sessions
+     * @param {Object} object - data to save in storage
+     *
+     * @example
+     * simpleStorage.setSync({
+     *   key: value,
+     *   Key2: value
+     * });
+     */
+    setSync: function (object) {
+      if (object && typeof object === 'object') {
+        if (this.isChromeApp) {
+          chrome.storage.sync.set(object);
+        } else if (this.hasLocalStorage) {
+          for (var key in object) {
+            localStorage[key] = JSON.stringify({
+              value: object[key],
+              type: typeof object[key]
+            });
+          }
+        } else {
+          console.warn('localStorage is not supported in your browser');
+        }
+      } else {
+        throw new Error('data must be an object');
+      }
+    },
+
+    /**
+     * chrome.storage.local api if avaliable fallback to localStorage
+     * saves are local only
+     * @param {Object} object - data to save in storage
+     *
+     * @example
+     * simpleStorage.setLocal({
+     *   key: value,
+     *   Key2: value
+     * });
+     */
+    setLocal: function (object) {
+      if (object && typeof object === 'object') {
+        if (this.isChromeApp) {
+          chrome.storage.local.set(object);
+        } else if (this.hasLocalStorage) {
+          for (var key in object) {
+            localStorage[key] = JSON.stringify({
+              value: object[key],
+              type: typeof object[key]
+            });
+          }
+        } else {
+          console.warn('localStorage is not supported in your browser');
+        }
+      } else {
+        throw new Error('data must be an object');
+      }
+    }
+  };
+
+  return simpleStorage;
+}());
